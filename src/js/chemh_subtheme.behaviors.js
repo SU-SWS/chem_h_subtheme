d630607bd692c75b28718afeed6b8959ffc3ad80
jdwjdwjdw <jacobdeanwilson@gmail.com>
2023-04-14 15:58:11 +0000

    Update search and search-results page

--- src/js/chemh_subtheme.behaviors.js
+++ src/js/chemh_subtheme.behaviors.js
@@ -15,9 +15,15 @@
 
 
     (function ($) {
-      const menuSearchButton = $('<button>', {class: 'menu-search-button', 'id': 'search-button-toggle'}).html('').click(() => {
+      // const menuSearchButton = $('<button>', {class: 'menu-search-button', 'id': 'search-button-toggle'}).html('').click(() => {
+      //   $('#block-chem-h-subtheme-search form').toggleClass('show-form');
+      // })
+      const menuSearchButton = $('<button>', {class: 'menu-search-button', 'id': 'search-button-toggle', 'aria-expanded': 'false'}).html('').click(() => {
+        $('.menu-search-button').toggleClass('menu-search-button-expanded');
         $('#block-chem-h-subtheme-search form').toggleClass('show-form');
+        // $(this).attr( 'aria-expanded', menuSearchButton.attr('aria-expanded') === 'true' ? false : true);
       })
+      // $('.su-multi-menu__menu').append(menuSearchButton)
       $('.su-multi-menu--dropdowns').append(menuSearchButton)
       $('.su-brand-bar,.su-masthead').wrapAll('<div class="fixed-header">');
 
